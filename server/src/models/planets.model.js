abdcf1c9a91323eed05017a21ddb18df93cc6bb8
RoiGaon <roigaon@gmail.com>
2022-10-09 18:29:42 +0300

    filter the planets to contain only the name

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -59,7 +59,7 @@ async function savePlanet(planet) {
 }
 
 async function getAllPlanets() {
-  return await planets.find({});
+  return await planets.find({}, { __v: 0, _id: 0 });
 }
 
 module.exports = {
