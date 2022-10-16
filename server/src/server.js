b1c8ea249ee6eb19675380063678340c88ec59bc
RoiGaon <roigaon@gmail.com>
2022-10-16 14:28:51 +0300

    adding the mongo connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,15 @@
 const http = require("http");
+
+const { mongoConnect } = require("./services/mongo");
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 8080;
+
 const server = http.createServer(app);
 
 async function startServer() {
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
---
+++ server/src/services/mongo.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.once("error", (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  await mongoose.connect(process.env.MONGO_CONNECTION_URL);
+}
+
+async function mongoDisConnect() {
+  await mongoose.disconnect();
+}
+
+module.exports = { mongoConnect, mongoDisConnect };
