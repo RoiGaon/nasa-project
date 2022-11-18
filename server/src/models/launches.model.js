262b545066db942946c1a27b4297f98de493ce9b
RoiGaon <roigaon@gmail.com>
2022-11-18 14:30:44 +0200

    adding pagination to my api

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -99,8 +99,12 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
-  return await launchesDataBase.find({}, { _id: 0, _v: 0 });
+async function getAllLaunches(skip, limit) {
+  return await launchesDataBase
+    .find({}, { _id: 0, _v: 0 })
+    .sort({ flightNumber: 1 })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function scheduleNewLaunch(launch) {
